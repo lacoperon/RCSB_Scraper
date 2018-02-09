a11b55e63aa541578d02da3e9229a28dbc3749fa
Elliot <elliot.james.williams@gmail.com>
2018-02-09 16:54:49 -0500

    Added successful scraping code for structure PDB and structure links

--- scraper.R
+++ scraper.R
@@ -1,16 +1,20 @@
-library(httr)
 library(XML)
 library(readr)
-library(purrr)
 
-url <- "http://www.rcsb.org/pdb/results/results.do?tabtoshow=Current&qrid=58258A08"
-s <- GET(url)
-w <- content(s, as='text') # converts s to plaintext of HTML
-d <- htmlParse(file=content(s, as="text", asText=T))
+# Opens pre-saved version of the search results
+searchResults <- read_file("data/searchResults.html")
+d <- htmlParse(searchResults)
 
 # Gets the number of structures returned
 node_num_struct <- getNodeSet(doc=d, path="//ul/li[@class='active']//a")[[1]]
 num_structure <- trimws(xmlToList(node_num_struct)$text)
 
-# Scrapes all search result HTML nodes
-result_nodes <- getNodeSet(doc=d, path="//ul[@class='list-unstyled SearchResultsBlock']")
+# Scrapes all search result HTML nodes (and their PDB accession numbers)
+node_set_path <- "//ul[@id='SearchResultsDetails-MainContent']/li"
+result_nodes <- getNodeSet(doc=d, path=node_set_path)
+cand_structs  <- data.frame(sapply(result_nodes, function(node) {substr(xmlAttrs(node)[[1]], 11, 1000)}))
+colnames(cand_structs) <- c("PDB_Number")
+
+# Scrapes all search result links (to the PDB structures themselves)
+file_link_path <- "//ul[@id='SearchResultsDetails-MainContent']/li/div[2]/div[1]/div/a[1]"
+cand_structs$Link   <- xpathSApply(doc=d, path=file_link_path, fun=xmlGetAttr, name="href")
