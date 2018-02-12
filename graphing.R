409c4c4fe3b3d49ce2306c7df091914cb65b415d
Elliot <elliot.james.williams@gmail.com>
2018-02-12 11:28:41 -0500

    Added more (and prettier) exploratory graphing

--- graphing.R
+++ graphing.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(dplyr)
 
 # Plot the resolution of all candidate structures
 ggplot(cand_structs2, aes(Resolution)) + geom_density(adjust = 2/3, fill="skyblue", color="skyblue", alpha=0.3) +
@@ -14,10 +15,16 @@ ggplot(cand_structs2, aes(mRNA_Length)) + geom_density(adjust = 1/3, fill="red",
        title="Amount of mRNA in Filtered RCSB Structures w mRNA, S12")
 ggsave(filename="./figures/mRNALength.png")
 
+MethodData <- cand_structs2 %>%
+  group_by(Method) %>%
+  summarize(count = n()) %>%
+  mutate(pct = count / sum(count))
+
 # Plot how the various candidate structures are imaged
-ggplot(cand_structs2, aes(x=Method, fill=Method)) + geom_bar() + 
+ggplot(MethodData, aes(x=Method, fill=Method, y=pct)) + geom_bar(stat="identity") + 
   labs(y="Count of Structures", 
-       title="How the Candidate Filtered RCSB Structures are Imaged")
+       title="How the Candidate Filtered RCSB Structures are Imaged") +
+  geom_text(aes(label=paste("n=", scales::comma(count), sep=""), vjust=2), color="white")
 ggsave(filename="./figures/imaging.png")
 
 library(stringr)
@@ -36,5 +43,18 @@ df <- data.frame(labels, freq_counts, counts)
 
 ggplot(df, aes(x=labels, y=freq_counts, fill=labels)) + geom_bar(stat="identity") + 
   labs(y="Frequency of Nucleotide", 
-       title="Nucleotide Frequency in Structures")
+       title="Nucleotide Frequency in Structures") +
+  geom_text(aes(label=paste("n=", scales::comma(counts), sep=""), vjust=2), color="white")
+
 ggsave("./figures/nucleotide_frequencies.png")
+
+# df <- df%>%
+#   dplyr::arrange(counts) %>%
+#   mutate(labels=factor(labels, levels =labels))
+# 
+# ggplot(df, aes(x=labels, y=freq_counts, fill=labels)) + geom_bar(stat="identity") + 
+#   labs(y="Frequency of Nucleotide", 
+#        title="Nucleotide Frequency in Structures") +
+#   geom_text(aes(label=paste("n=", scales::comma(counts), sep=""), vjust=2), color="white")
+# 
+# ggsave("./figures/nucleotide_frequencies_ordered.png")
